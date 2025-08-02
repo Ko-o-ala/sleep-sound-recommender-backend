6c054f8cd06ef088c6cd525e8261ede7d426edb6
sodaaw <ghk132435@naver.com>
2025-08-02 14:29:56 +0900

    임베딩 모델 변경

--- scripts/embed_generator.py
+++ scripts/embed_generator.py
@@ -3,7 +3,7 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer("BAAI/bge-large-en-v1.5")
+model = SentenceTransformer("BAAI/bge-small-en-v1.5")
 
 # 주어진 입력(자연어)를 384차원의 임베딩 벡터로 변환
 def generate_embedding(text: str) -> np.ndarray:
